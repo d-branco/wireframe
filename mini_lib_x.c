98d87580482f47a9f5932f14bbb2e75b2ca84c7c
gris <185072671+d-branco@users.noreply.github.com>
2025-02-10 09:34:55 +0000

    one red pixel

--- mini_lib_x.c
+++ mini_lib_x.c
@@ -6,18 +6,44 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 08:13:26 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/02/10 09:10:32 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/02/10 09:33:40 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_window;
+	t_data	img;
+
+	//int offset = (y * line_length + x * (bits_per_pixel / 8));
 
 	mlx = mlx_init();
 	mlx_window = mlx_new_window(mlx, 1920, 1080, "Hello ground!");
+	img.img = mlx_new_image(mlx, 1920, 1820);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_window, img.img, 0, 0);
 	mlx_loop(mlx);
+}
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
 }
